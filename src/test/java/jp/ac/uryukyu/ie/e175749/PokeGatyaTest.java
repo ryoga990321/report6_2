3c52bc64b3f8118659dbf23d1b05230401f707ea
Ryoga Gushiken <e175749@ie.u-ryukyu.ac.jp>
2018-01-14 16:26:46 +0900

    third commit

---
+++ src/test/java/jp/ac/uryukyu/ie/e175749/PokeGatyaTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e175749;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PokeGatyaTest {
+    @Test
+    public void choice() {
+        String[] ssr = {"フリーザー", "ファイアー", "サンダー"};
+        PokeGatya gatya=new PokeGatya();
+        gatya.choice(0,ssr);
+        assertEquals(gatya.monster,"フリーザー");
+        gatya.choice(1,ssr);
+        assertEquals(gatya.monster,"ファイアー");
+        gatya.choice(2,ssr);
+        assertEquals(gatya.monster,"サンダー");
+    }
+
+}
